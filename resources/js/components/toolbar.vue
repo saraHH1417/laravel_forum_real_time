8c85f7c5889fa72da0c8b21ae929bfcfe349dcbd
SARAHH1417 <sara.afzar@gmail.com>
2021-12-05 13:47:15 +0330

    Vuetify , Vue Router and Initial Components

---
+++ resources/js/components/toolbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-card
+        color="grey lighten-4"
+        flat
+        height="200px"
+        tile
+    >
+        <v-toolbar extended>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+            <v-toolbar-title>Title</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+            <v-btn flat>
+                Forum
+            </v-btn>
+
+            <v-btn flat>
+                Ask Question
+            </v-btn>
+
+            <v-btn flat>
+                Category
+            </v-btn>
+
+            <v-btn flat>
+                Login
+            </v-btn>
+        </v-toolbar>
+    </v-card>
+</template>
+<script>
+
+</script>
+
+<style>
+
+</style>
